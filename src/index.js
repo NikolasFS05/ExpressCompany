346cd127435a1bab13a053a14913ddd50e47d2b6
NicolasFS <nicolas.fonsecas@konradlorenz.edu.co>
2022-04-12 17:19:34 -0500

    Creación del index
    Se han importado los módulos necesarios, además de crear las rutas de acceso hacia los endpoints de los archivos "registro" y "factura".

---
+++ src/index.js
@@ -0,0 +1,30 @@
+//Importación de modulos
+const bodyparser = require("body-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//Importación de las rutas
+const registroRoutes = require("./routes/registro");
+const facturaRoutes = require("./routes/factura");
+
+//Creación del puerto y uso de express
+const port = 3000;
+const app = express();
+
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+
+//Creación del intermediario hacia las rutas
+app.use('/',registroRoutes);
+app.use('/', facturaRoutes);
+
+//Conexión de la base de datos
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("Conexión exitosa"))
+    .catch((error) => console.log(error));
+
+//Conexión con el puerto
+app.listen(port, () => {
+    console.log("Puerto en uso: " +port);
+});
