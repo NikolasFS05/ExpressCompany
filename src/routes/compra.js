e127ea95182985057116fc29aac29eb084939e1c
AcevedoCristian <cristiangonzalez1030@hotmail.com>
2022-04-15 16:12:28 -0500

    Modificación de variables
    Se modificaron las variables y rutas.

--- src/routes/compra.js
+++ src/routes/compra.js
@@ -1,36 +1,36 @@
 //Importación de modulos
 const express = require("express");
-const facturaSchema = require("../models/compra");
+const compraSchema = require("../models/compra");
 const router = express.Router();
 
 //Creación de EndPoints
 //Petición Get
-router.get("/compra", (req, res) => {
-    facturaSchema.find()
+router.get("/ingreso", (req, res) => {
+    compraSchema.find()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
 
 //Petición Get con parametro
-router.get("/compra/:id", (req, res) => {
+router.get("/ingreso/:id", (req, res) => {
     const { id } = req.params;
-    facturaSchema.find({_id:id})
+    compraSchema.find({_id:id})
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
 
 //Petición Post
-router.post("/compra", (req, res) => {
-    const factura = facturaSchema(req.body);
-    factura .save() .then((data) => res.json(data))
+router.post("/ingreso", (req, res) => {
+    const ingreso = compraSchema(req.body);
+    ingreso .save() .then((data) => res.json(data))
         .catch((error) => res.json({message: error}));
 });
 
 //Petición Put
-router.put("/compra/:id", (req, res) => {
+router.put("/ingreso/:id", (req, res) => {
     const { id } = req.params;
     const { usuario, producto, cuponDescuento ,precio, Totalprecio, metodoPago} = req.body;
-    facturaSchemaSchema.updateOne({ _id: id }, {
+    compraSchema.updateOne({ _id: id }, {
         $set: { usuario, producto, cuponDescuento ,precio, Totalprecio, metodoPago}
     })
         .then((data) => res.json(data))
@@ -38,9 +38,9 @@ router.put("/compra/:id", (req, res) => {
 });
 
 //Petición Delete
-router.delete("/compra/:id", (req, res) =>{
+router.delete("/ingreso/:id", (req, res) =>{
     const { id } = req.params;
-    facturaSchema.deleteOne({_id:id})
+    compraSchema.deleteOne({_id:id})
         .then((data) => res.json(data))
         .catch((error) => res.json({ message : error}));
 });
---
+++ src/routes/ingreso.js
@@ -0,0 +1,46 @@
+//Importación de modulos
+const express = require("express");
+const ingresoSchema = require("../models/compra");
+const router = express.Router();
+
+//Creación de EndPoints
+//Petición Get
+router.get("/ingreso", (req, res) => {
+    ingresoSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//Petición Get con parametro
+router.get("/ingreso/:id", (req, res) => {
+    const { id } = req.params;
+    ingresoSchema.find({_id:id})
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//Petición Post
+router.post("/ingreso", (req, res) => {
+    const ingreso = ingresoSchema(req.body);
+    ingreso .save() .then((data) => res.json(data))
+        .catch((error) => res.json({message: error}));
+});
+
+//Petición Put
+router.put("/ingreso/:id", (req, res) => {
+    const { id } = req.params;
+    const { usuario, contrasena} = req.body;
+    ingresoSchema.updateOne({ _id: id }, {
+        $set: { usuario, contrasena}
+    })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//Petición Delete
+router.delete("/ingreso/:id", (req, res) =>{
+    const { id } = req.params;
+    ingresoSchema.deleteOne({_id:id})
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message : error}));
+});
